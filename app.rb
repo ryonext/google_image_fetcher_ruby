73b0c0566a71aed65363b3e6c360431cd021daec
ryonext <ryonext.s@gmail.com>
2016-02-12 13:59:30 +0900

    Use image search.

--- app.rb
+++ app.rb
@@ -7,21 +7,15 @@ query = ARGV[0]
 
 api_key = ENV["GOOGLE_API_KEY"]
 engine_id = ENV["SEARCH_ENGINE_ID"]
-url = "https://www.googleapis.com/customsearch/v1?key=#{api_key}&cx=#{engine_id}&q=#{query}"
 
+url = "https://www.googleapis.com/customsearch/v1?key=#{api_key}&cx=#{engine_id}&q=#{query}&searchType=image"
 conn = Faraday.new(url: URI.encode(url))
 response = conn.get
 result = JSON.parse(response.body)
 
-
-image_url = result["items"].map do |item|
-  if item["pagemap"] && item["pagemap"]["cse_image"]
-    item["pagemap"]["cse_image"].first["src"]
-  end
-end.compact
+image_url = result["items"].map {|item| item["link"] }
 
 dir_path = "data/#{query}"
-
 FileUtils.mkdir_p(dir_path)
 
 image_url.each do |url|
