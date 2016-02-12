1413db6d76ed1325bfe05c0818521936d630504c
ryonext <ryonext.s@gmail.com>
2016-02-12 11:36:46 +0900

    Save image file which are searched.

--- app.rb
+++ app.rb
@@ -1,11 +1,34 @@
 require "bundler/setup"
 Bundler.require
 
+require "json"
+require "open-uri"
+
 api_key = ENV["GOOGLE_API_KEY"]
 engine_id = ENV["SEARCH_ENGINE_ID"]
 query = "寿司"
-url = "https://www.googleapis.com/customsearch/v1?key=#{api_key}&cx=#{engine_id}&q=query"
+url = "https://www.googleapis.com/customsearch/v1?key=#{api_key}&cx=#{engine_id}&q=#{query}"
 
-conn = Faraday.new(url: url)
+conn = Faraday.new(url: URI.encode(url))
 response = conn.get
-puts response.body
+result = JSON.parse(response.body)
+
+
+image_url = result["items"].map do |item|
+  if item["pagemap"]
+    item["pagemap"]["cse_image"].first["src"]
+  end
+end.compact
+
+dir_path = "data/#{query}"
+
+FileUtils.mkdir_p(dir_path)
+
+image_url.each do |url|
+  filename = "#{dir_path}/#{File.basename(url)}"
+  open(filename, 'wb') do |file|
+    open(url) do |data|
+      file.write(data.read)
+    end
+  end
+end
